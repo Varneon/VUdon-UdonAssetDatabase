010cdb18c36eb07ee1664e1b185179eb33ad15c3
Varneon <me@varneon.com>
2023-02-24 04:49:31 -0800

    Add database scope enum

---
+++ Packages/com.varneon.vudon.udon-asset-database/Runtime/UdonAssetDatabaseScope.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.UdonAssetDatabase.Enums
+{
+    public enum UdonAssetDatabaseScope
+    {
+        /// <summary>
+        /// Include everything from the current project
+        /// </summary>
+        /// <remarks>ONLY USE THIS IF NECESSARY!</remarks>
+        [InspectorName("Project (Only if necessary!)")]
+        Project,
+
+        /// <summary>
+        /// Include everything in the current scene
+        /// </summary>
+        [InspectorName("Scene (Recommended)")]
+        Scene,
+
+        /// <summary>
+        /// Include everything under provided GameObjects
+        /// </summary>
+        Roots
+    }
+}
