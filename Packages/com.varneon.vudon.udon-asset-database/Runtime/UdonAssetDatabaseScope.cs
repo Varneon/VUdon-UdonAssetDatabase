4ce5ace1eada010476cfa3749c6379a875d4d520
Varneon <me@varneon.com>
2023-02-24 06:02:14 -0800

    Remove "Project" database scope option
    Indexing the entire project is not a reasonable option for this uses case

--- Packages/com.varneon.vudon.udon-asset-database/Runtime/UdonAssetDatabaseScope.cs
+++ Packages/com.varneon.vudon.udon-asset-database/Runtime/UdonAssetDatabaseScope.cs
@@ -5,13 +5,6 @@ namespace Varneon.VUdon.UdonAssetDatabase.Enums
     public enum UdonAssetDatabaseScope
     {
         /// <summary>
-        /// Include everything from the current project
-        /// </summary>
-        /// <remarks>ONLY USE THIS IF NECESSARY!</remarks>
-        [InspectorName("Project (Only if necessary!)")]
-        Project,
-
-        /// <summary>
         /// Include everything in the current scene
         /// </summary>
         [InspectorName("Scene (Recommended)")]
